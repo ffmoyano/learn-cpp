750d36456eecc29b1b427cdce49a8e4ecf44bb9a
ffmoyano <ffmoyano@outlook.com>
2023-04-20 22:16:20 +0200

    lesson 6_3 which somehow was not saved last time

--- 06_scope_duration_linkage/06_03_linkage/main.cpp
+++ 06_scope_duration_linkage/06_03_linkage/main.cpp
@@ -1,3 +1,25 @@
+// EXTERNAL LINKAGE -> these identifiers can be used in multiple files
+// External global variable definitions -> this variables are initialized here and can be used with this value in another file
+int g_x;                       // defines non-initialized external global variable (zero initialized by default)
+extern const int g_x{ 1 };     // defines initialized const external global variable
+extern constexpr int g_x{ 2 }; // defines initialized constexpr external global variable
+
+// Forward declarations -> this variables are initialized in another file and are declared here with the value of the other file
+extern int g_y;                // forward declaration for non-constant global variable
+extern const int g_y;          // forward declaration for const global variable
+extern constexpr int g_y;      // not allowed: constexpr variables can't be forward declared
+
 int main() {
 
 }
+
+// INTERNAL LINKAGE -> these identifiers can only be used in this file
+// Internal global variables definitions:
+static int g_x;          // defines non-initialized internal global variable (zero initialized by default)
+static int g_x{ 1 };     // defines initialized internal global variable
+
+const int g_y { 2 };     // defines initialized internal global const variable
+constexpr int g_y { 3 }; // defines initialized internal global constexpr variable
+
+// Internal function definitions:
+static int foo() {};     // defines internal function
