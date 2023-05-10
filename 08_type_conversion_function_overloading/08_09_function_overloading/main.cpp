31720c625ebfb7b9f2c05ad5c41af2dfb2205383
ffmoyano <ffmoyano@outlook.com>
2023-05-10 23:25:27 +0200

    modification of 08_09

--- 08_type_conversion_function_overloading/08_09_function_overloading/main.cpp
+++ 08_type_conversion_function_overloading/08_09_function_overloading/main.cpp
@@ -16,10 +16,25 @@ double add(double x, double y)
 //
 //}
 
+void print(int)
+{
+}
+
+void print(double)
+{
+}
+
 int main()
 {
     std::cout << add(1, 2); // calls add(int, int)
     std::cout << '\n';
     std::cout << add(1.2, 3.4); // calls add(double, double)
+
+    // Matches made by applying numeric promotions take precedence over any matches made by applying numeric conversions.
+    print('a'); // promoted to match print(int)
+    print(true); // promoted to match print(int)
+    print(4.5f); // promoted to match print(double)
+
+    // print(5L); // 5L is type long -> no match from a given step is considered to be better than any other match from the same step.
 
 }
