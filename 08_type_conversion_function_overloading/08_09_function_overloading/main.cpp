efa45798b95c0fbc401dbbc5ae203ba1b860d77f
ffmoyano <ffmoyano@outlook.com>
2023-05-10 23:21:29 +0200

    modification of 08_09

--- 08_type_conversion_function_overloading/08_09_function_overloading/main.cpp
+++ 08_type_conversion_function_overloading/08_09_function_overloading/main.cpp
@@ -10,6 +10,12 @@ double add(double x, double y)
     return x + y;
 }
 
+// A function’s return type is not considered when differentiating overloaded functions. This function is not admitted
+// because the compiler thinks is the same than the previously declared function
+//int add(double x, double y)  {
+//
+//}
+
 int main()
 {
     std::cout << add(1, 2); // calls add(int, int)
